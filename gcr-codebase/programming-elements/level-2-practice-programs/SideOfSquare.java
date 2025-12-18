4a6460505201f8a112bbd7bd009d522bed34eeb4
aditya-kokashe <adi310kokashe@gmail.com>
2025-12-18 12:29:01 +0530

    [Aditya Kokashe] : added a program to find the side of the square whose parimeter you read from the user

---
+++ gcr-codebase/programming-elements/level-2-practice-programs/SideOfSquare.java
@@ -0,0 +1,17 @@
+//Write a program to find the side of the square whose parimeter you read from the user 
+
+import java.util.Scanner;
+public class SideOfSquare{
+	public static void main(String[] args){
+		
+		//taking perimeter input from user
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Perimeter Of Square: ");
+		double parimeterOfSquare = sc.nextDouble();
+		//Perimeter of the Square is 4 times the side
+		double sideOfSquare = parimeterOfSquare / 4;
+		
+		//displaying the resulted side of square
+		System.out.println(sideOfSquare);
+	}
+}	
