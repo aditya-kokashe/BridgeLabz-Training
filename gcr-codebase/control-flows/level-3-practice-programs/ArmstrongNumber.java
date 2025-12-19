25e4f5d7dfca1d13455426bf0b503b4503928b18
aditya-kokashe <adi310kokashe@gmail.com>
2025-12-19 12:44:41 +0530

    [Aditya Kokashe] : created an Armstrong number program which is a number whose Sum of cubes of each digit results in the original number.

---
+++ gcr-codebase/control-flows/level-3-practice-programs/ArmstrongNumber.java
@@ -0,0 +1,28 @@
+//Create a program to check if a number is armstrong or not. Use the hints to show the steps clearly in the code
+
+import java.util.Scanner;
+public class ArmstrongNumber{
+	public static void main(String[] agrs){
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number: ");
+		int number= sc.nextInt();
+		int sum = 0;
+		int originalNumber = number;
+		//Check quotient and reminder
+		while(originalNumber != 0){
+			int digit = originalNumber%10;
+			sum += digit*digit*digit;
+			originalNumber /= 10;
+		}	
+		
+		
+		//Display the results
+		if(number == sum){
+			System.out.println("The number is an Armstrong Number!!");
+		}else{
+			System.out.println("The number is Not an Armstrong Number");
+		}
+	}
+}
