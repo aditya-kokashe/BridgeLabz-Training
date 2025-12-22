d9c0e67d4454ab8eeb2f1608be15e67a6c0ef79e
aditya-kokashe <adi310kokashe@gmail.com>
2025-12-22 20:14:23 +0530

    [Aditya Kokashe] : created a Null pointer exception program to demonstrate null pointer exception

---
+++ gcr-codebase/strings/level-1-practice-programs/NullPointerDemo.java
@@ -0,0 +1,32 @@
+//Write a program to demonstrate NullPointerException. 
+
+public class NullPointerDemo{
+    //Generate NullPointerException
+    public static void generateException(){
+        String text = null;
+        System.out.println(text.length());
+    }
+    //Handle NullPointerException using try-catch
+    public static void handleException() {
+        try {
+            String text = null;
+            System.out.println(text.length());
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException handled");
+        }
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("Generating NullPointerException:");
+		
+		
+        try{
+            generateException();
+        }catch(NullPointerException e){
+            System.out.println("Exception occurred in generateException() method.");
+        }
+        System.out.println("Handling NullPointerException:");
+        handleException();
+    }
+}
