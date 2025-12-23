9b06e80b9369d300f98c5ff6cdfcd1e05ff1fe1b
aditya-kokashe <adi310kokashe@gmail.com>
2025-12-23 17:10:41 +0530

    [Aditya Kokashe] : created a program to reverse a string using a for loop.

--- gcr-codebase/extras/string-handling/ReverseString.java
+++ gcr-codebase/extras/string-handling/ReverseString.java
@@ -2,15 +2,20 @@ import java.util.Scanner;
 public class ReverseString{
 	public static String reverse(String text){
 		String reversed = " ";
+		//loop thorough the String 
 		for(int i=text.length()-1; i>=0; i--){
 			reversed += text.charAt(i);
 		}
 		return reversed;
 	}
+	
+	
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter a text: ");
 		String text = sc.next();
+		
+		//Display the text as reverse
 		System.out.println(reverse(text));
 	}
 }
