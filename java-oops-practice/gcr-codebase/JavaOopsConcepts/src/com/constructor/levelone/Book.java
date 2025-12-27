008c70c3abb08eab34dd1d03276aae9707b26b37
aditya-kokashe <adi310kokashe@gmail.com>
2025-12-27 22:26:36 +0530

    [Aditya Kokashe] : created a program to display book details usiing default and parameterized constructor

--- java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/levelone/Book.java
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/levelone/Book.java
@@ -26,16 +26,4 @@ public class Book {
 		System.out.println("Book price: "+ price);
 	}
 
-	public static void main(String[] args) {
-		//Default constructor
-		Book book1 = new Book();
-		System.out.println("Book 1: ");
-		book1.displayBookDetails();
-		
-		//Parameterized constructor
-		Book book2 = new Book("Krishna Key", "Ashwin Sanghi", 1230);
-		System.out.println("Book 2: ");
-		book2.displayBookDetails();
-	}
-
 }
