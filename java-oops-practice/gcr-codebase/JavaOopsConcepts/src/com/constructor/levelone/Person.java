747019fa7a58eb1dbd3633dd0bb70754059c7a3f
aditya-kokashe <adi310kokashe@gmail.com>
2025-12-27 22:40:08 +0530

    [Aditya Kokashe] : created a Person class with a copy constructor that clones another person's attributes.

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/levelone/Person.java
@@ -0,0 +1,31 @@
+package com.constructor.levelone;
+
+public class Person {
+	
+	//Attributes
+	double height;
+	double weight;
+	String hair;
+	
+	//Default constructor
+	public Person() {
+		height = 120;
+		weight = 50;
+		hair = "white";
+	}
+	
+	//Copy constructor
+	public Person(Person previousPerson) {
+		this.height = previousPerson.height;
+		this.weight = previousPerson.weight;
+		this.hair = previousPerson.hair;
+	}
+	
+	void displayDetails() {
+		System.out.println("Height: "+height);
+		System.out.println("Weight: "+weight);
+		System.out.println("hair color: "+hair);
+		System.out.println("----------------------");
+	}
+
+}
