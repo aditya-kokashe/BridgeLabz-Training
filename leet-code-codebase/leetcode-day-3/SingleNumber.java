d4ff2467932cfd57446096470c1b7d700f3eb655
aditya-kokashe <adi310kokashe@gmail.com>
2025-12-29 20:35:49 +0530

    [Aditya Kokashe] : solved LeetCode 136 - Single Number

---
+++ leet-code-codebase/leetcode-day-3/SingleNumber.java
@@ -0,0 +1,11 @@
+// Leetcode 136
+class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
