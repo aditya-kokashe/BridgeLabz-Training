c1eaad0c4aeb28381ac49743d2eea2648ffdd7c6
aditya-kokashe <adi310kokashe@gmail.com>
2025-12-30 23:37:57 +0530

    [Aditya Kokashe] : added Employee class for employee name

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/objectmodeling/levelone/Employee.java
@@ -0,0 +1,12 @@
+package com.objectmodeling.levelone;
+
+public class Employee {
+
+	String employeeName;
+
+	//Constructor
+    public Employee(String employeeName){
+        this.employeeName = employeeName;
+    }
+
+}
