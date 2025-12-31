467e5c8ec827ba134d1030b5cc78ea74285eb9f8
aditya-kokashe <adi310kokashe@gmail.com>
2025-12-31 23:24:47 +0530

    [Aditya Kokashe] : added Course base class with coursename, and duration attributes

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/educationalCourseHierarchy/Course.java
@@ -0,0 +1,16 @@
+package com.inheritance.educationalCourseHierarchy;
+
+public class Course {
+
+	String courseName;
+	int duration;
+	public Course(String courseName, int duration) {
+		this.courseName = courseName;
+		this.duration = duration;
+	}
+	public void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
