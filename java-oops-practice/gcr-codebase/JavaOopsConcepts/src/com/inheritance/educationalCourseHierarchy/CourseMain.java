9764e9d352bca314b0b941959958aa57031dd3d2
aditya-kokashe <adi310kokashe@gmail.com>
2025-12-31 23:28:11 +0530

    [Aditya Kokashe] : added Course Main class to demonstrate multilevel inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/educationalCourseHierarchy/CourseMain.java
@@ -0,0 +1,12 @@
+package com.inheritance.educationalCourseHierarchy;
+
+public class CourseMain {
+
+	public static void main(String[] args) {
+
+		Course a = new PaidOnlineCourse("Java", 1, "Online", "Yes", 1000, 100);
+		a.display();
+
+	}
+
+}
