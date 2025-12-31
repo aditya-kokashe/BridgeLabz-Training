3767f3028d4fae9a20ef8c472b580c918162eeb8
aditya-kokashe <adi310kokashe@gmail.com>
2025-12-31 22:28:43 +0530

    [Aditya Kokashe] : added Book main class to demonstrate single inheritance

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/libraryManagement/BookMain.java
@@ -0,0 +1,11 @@
+package com.inheritance.libraryManagement;
+
+public class BookMain {
+
+	public static void main(String[] args) {
+
+		Book a = new Author("Atomic Thinking", 2025, "James Marshden", "Self help");
+		a.displayInfo();
+	}
+
+}
