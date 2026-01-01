e833db9f8f31f263105959d8be343c5c7a469fa7
aditya-kokashe <adi310kokashe@gmail.com>
2026-01-01 11:51:44 +0530

    [Aditya Kokashe] : added Vehicle super class with model and maxSpeed attribute and charge and refuel methods

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/vehicleMgmtSystem/Vehicle.java
@@ -0,0 +1,23 @@
+package com.inheritance.vehicleMgmtSystem;
+
+public class Vehicle {
+
+	int maxSpeed;
+	String model;
+	public Vehicle(String model, int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+		this.model = model;
+		
+	}
+	
+	public void charge() {
+			
+		
+	}
+
+	public void refuel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
