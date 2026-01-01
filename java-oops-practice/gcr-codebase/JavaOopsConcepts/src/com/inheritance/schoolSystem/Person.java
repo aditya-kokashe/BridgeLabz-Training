04a835a0246be2d3c91ec77b3edf93b0ce7679fc
aditya-kokashe <adi310kokashe@gmail.com>
2026-01-01 11:22:14 +0530

    [Aditya Kokashe] : added Person superclass with name and age attributes

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/inheritance/schoolSystem/Person.java
@@ -0,0 +1,16 @@
+package com.inheritance.schoolSystem;
+
+public class Person {
+
+	String name;
+	int age;
+	public Person(String name, int age) {
+		this.age = age;
+		this.name = name;
+		
+	}
+	public void displayRole() {
+		
+	}
+
+}
