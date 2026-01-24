020481c13e291466b86ace468b80346c14b8682a
aditya-kokashe <adi310kokashe@gmail.com>
2026-01-25 00:40:38 +0530

    [Aditya Kokashe] : added JUnit test cases for BankAccount transactions with balance validation

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/junit/bankaccount/BankAccount.java
@@ -0,0 +1,24 @@
+package com.junit.bankaccount;
+
+public class BankAccount {
+
+    private double balance = 0;
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Invalid deposit amount");
+        }
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            throw new IllegalArgumentException("Insufficient funds");
+        }
+        balance -= amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
