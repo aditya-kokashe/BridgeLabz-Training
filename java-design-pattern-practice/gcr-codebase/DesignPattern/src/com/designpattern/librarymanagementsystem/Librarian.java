d7a8ccddb961f4ca6334a965590285c2d9cea9c5
aditya-kokashe <adi310kokashe@gmail.com>
2026-02-05 13:42:19 +0530

    [Aditya Kokashe] : added Librarian.java program to manage library operations and catalog updates

---
+++ java-design-pattern-practice/gcr-codebase/DesignPattern/src/com/designpattern/librarymanagementsystem/Librarian.java
@@ -0,0 +1,8 @@
+package com.designpattern.librarymanagementsystem;
+
+
+public class Librarian implements User {
+    public void getRole() {
+        System.out.println("Librarian: Manages inventory");
+    }
+}
